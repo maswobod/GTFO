8f0ebca09ff84a145eadc96b1b9c68035943c300
Jonas Graf <Jonas@MacBook-Pro.local>
2015-11-24 10:33:28 +0100

    fixed Prototyp
    fixed Enemy Ai by Patrick
    set door Collider to IgnoreRaycast
    Add Score elem to Scene (fixed Error on Start)

--- GTFO/Assets/Scripts/EnemyAI.cs
+++ GTFO/Assets/Scripts/EnemyAI.cs
@@ -13,6 +13,7 @@ public class EnemyAI : MonoBehaviour
 	public float fieldOfViewAngle = 180;
 	private SphereCollider col;
 	private GameObject player;
+	private GameController gameController;
 
 	void Start ()
 	{
@@ -24,12 +25,14 @@ public class EnemyAI : MonoBehaviour
 		GotoNextPoint ();
 
 		player = GameObject.FindGameObjectWithTag ("Player");
+		gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
 		fpsController = player.GetComponent<FirstPersonController> ();
 		col = GetComponent<SphereCollider> ();
 	}
 
 	void Update ()
 	{
+		Debug.Log (this.gameObject.transform.up);
 		if (Vector3.Distance (transform.position, player.transform.position) > hearingDistance || fpsController.IsSneaking ()) { 
 			/*when not in seight make patrol*/
 			if(agent.remainingDistance <0.5f){
@@ -37,7 +40,8 @@ public class EnemyAI : MonoBehaviour
 			}
 		} else {
 			/*Follow the player*/
-			agent.destination = player.transform.position+Vector3.one;
+			//agent.destination = player.transform.position+Vector3.one;
+			agent.destination = player.transform.position+Vector3.up; /***Vector3.Up ist ein Vektor (0,1,0), Vector3.one ist (1,1,1)***/
 		}
 		
 	}
@@ -67,32 +71,48 @@ public class EnemyAI : MonoBehaviour
 			//playerInSight = false;
 			//Debug.Log("player");
 			// Create a vector from the enemy to the player and store the angle between it and forward.
-			Vector3 direction = other.transform.position - transform.forward;
+			//Vector3 direction = other.transform.position - transform.forward;
+			Vector3 direction = other.transform.position - (transform.position+Vector3.up); /***Vektor von dem Gegner + Vector3 nach oben, hin zur Position 
+			des Spielers. Das mit dem Vektor3.Up hab ich gemacht, weil der Pivot Punkt des Gegners an seinen Füßen ist und der des SPielers in der Mitte***/
+
+			Debug.DrawRay(transform.position+transform.up, direction.normalized*col.radius, Color.green); /***gut zum debuggen, 
+			hatte ich vergessen, dass es die Methode noch gibt ;) ***/
+
 			float angle = Vector3.Angle (direction, transform.forward);
 			// If the angle between forward and where the player is, is less than half the angle of view...
-			if (angle < fieldOfViewAngle * 0.5f) {
+			if (angle < fieldOfViewAngle * 0.5f) 
+			{
 				RaycastHit hit;
 				Debug.Log ("in Field of View");
 				// ... and if a raycast towards the player hits something...
 				//Debug.DrawRay(transform.position + transform.up +transform.forward, direction.normalized*10);
 				//if(Physics.Raycast(transform.position + transform.up +transform.forward, direction.normalized*10, out hit, col.radius))
-				if (Physics.Raycast (transform.position + transform.up, direction.normalized, out hit, col.radius)) {
+				//if (Physics.Raycast (transform.position + transform.up, direction.normalized, out hit, col.radius)) {
+				if (Physics.Raycast (transform.position + Vector3.up, direction.normalized, out hit, col.radius)) /*** +transform.up oder +Vector.up, 
+				sollte eigentlich egal sein welches von beiden man nimmt ***/
+				{ 
+
 					// ... and if the raycast hits the player...
 					//Debug.Log (hit.collider.gameObject);
 
-					if (hit.collider.gameObject == player.gameObject) {
+					if (hit.collider.gameObject == player.gameObject) 
+					{
 						// ... the player is in sight.
 						playerInSight = true;
-						transform.LookAt (player.transform.position);
+						//transform.LookAt (player.transform.position);/*** konnte komplett raus, der Agent macht das automatisch***/
 						Debug.Log ("And SEE you");
-
-						if(Vector3.Distance(player.transform.position,transform.position)<2.0f){
-							GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().endGameWithLoose();
+						agent.destination = player.transform.position; /*** An dieser Stelle noch das neue Target setzen. 
+						Sonst wird das bis jetzt nirgends getan (Zumindest soweit ich gesehn habe***/
+						if(Vector3.Distance(player.transform.position,transform.position)<2.0f)
+						{
+							gameController.endGameWithLoose(); //Besser in der Start() schon suchen
 						}
-
-					} else {
-						Debug.Log ("But something is in the way");
 					}
+				} 
+				else 
+				{
+					Debug.Log ("But something is in the way");
+					
 				}
 			}
 		}
