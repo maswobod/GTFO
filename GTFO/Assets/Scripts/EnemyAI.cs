e5051508a745be44b06ddf73695c67ffbe70c955
Jonas Graf <Jonas@MacBook-Pro.local>
2015-12-02 15:51:16 +0100

    no hearing while standing and bigger field of sight and enemy makes noise

--- GTFO/Assets/Scripts/EnemyAI.cs
+++ GTFO/Assets/Scripts/EnemyAI.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityStandardAssets.Characters.FirstPerson;
 
+[RequireComponent(typeof(AudioSource))]
 public class EnemyAI : MonoBehaviour
 {
 	public Transform[] points;
@@ -14,6 +15,10 @@ public class EnemyAI : MonoBehaviour
 	private SphereCollider col;
 	private GameObject player;
 	private GameController gameController;
+	public AudioClip[] footstepSounds;
+	private AudioSource audioSource;
+	public float stepInterval = 0.5f;
+	private float nextStep = 0f;
 
 	void Start ()
 	{
@@ -25,25 +30,25 @@ public class EnemyAI : MonoBehaviour
 		GotoNextPoint ();
 
 		player = GameObject.FindGameObjectWithTag ("Player");
-		gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+		gameController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
 		fpsController = player.GetComponent<FirstPersonController> ();
 		col = GetComponent<SphereCollider> ();
+		audioSource = GetComponent<AudioSource> ();
 	}
 
 	void Update ()
 	{
-		Debug.Log (this.gameObject.transform.up);
-		if (Vector3.Distance (transform.position, player.transform.position) > hearingDistance || fpsController.IsSneaking ()) { 
+		if (Vector3.Distance (transform.position, player.transform.position) > hearingDistance || fpsController.IsMakingNoise ()) { 
 			/*when not in seight make patrol*/
-			playerInSight=false;
-			if(agent.remainingDistance <0.5f){
+			playerInSight = false;
+			if (agent.remainingDistance < 0.5f) {
 				GotoNextPoint ();
 			}
 		} else {
 			/*Follow the player*/
-			//agent.destination = player.transform.position+Vector3.one;
-			agent.destination = player.transform.position+Vector3.up; /***Vector3.Up ist ein Vektor (0,1,0), Vector3.one ist (1,1,1)***/
-			playerInSight=true;		
+	
+			agent.destination = player.transform.position + Vector3.up; /***Vector3.Up ist ein Vektor (0,1,0), Vector3.one ist (1,1,1)***/
+			playerInSight = true;		
 		}
 		
 	}
@@ -54,7 +59,7 @@ public class EnemyAI : MonoBehaviour
 		if (points.Length == 0) {
 			return;
 		}
-		Debug.Log ("going to: " + destPoint);
+		//Debug.Log ("going to: " + destPoint);
 
 		// Set the agent to go to the currently selected destination.
 		agent.destination = points [destPoint].position;
@@ -64,59 +69,65 @@ public class EnemyAI : MonoBehaviour
 		destPoint = (destPoint + 1) % points.Length;
 	}
 
+	void playSounds (float distance, float maxDistance)
+	{
+		if (Time.time > nextStep) {
+			audioSource.volume = 1 - distance / maxDistance;
+			nextStep = Time.time + stepInterval; 
+			int n = Random.Range (1, footstepSounds.Length);
+			audioSource.clip = footstepSounds [n];
+			audioSource.PlayOneShot (audioSource.clip);
+			// move picked sound to index 0 so it's not picked next time
+			footstepSounds [n] = footstepSounds [0];
+			footstepSounds [0] = audioSource.clip;
+		}
+	}
+//	void stopSounds(){
+//		audioSource.Stop ();
+//	}
+
 	void OnTriggerStay (Collider other)
 	{
 		//Debug.Log ("on trigger stay: " + other.tag);
 		// If the player has entered the trigger sphere...
 		if (other.tag == "Player") {
-			// By default the player is not in sight.
-			//playerInSight = false;
-			//Debug.Log("player");
-			// Create a vector from the enemy to the player and store the angle between it and forward.
-			//Vector3 direction = other.transform.position - transform.forward;
-			Vector3 direction = other.transform.position - (transform.position+Vector3.up); /***Vektor von dem Gegner + Vector3 nach oben, hin zur Position 
+			float distance = Vector3.Distance (player.transform.position, transform.position);
+			playSounds (distance, col.radius);
+
+
+			Vector3 direction = other.transform.position - (transform.position + Vector3.up); /***Vektor von dem Gegner + Vector3 nach oben, hin zur Position 
 			des Spielers. Das mit dem Vektor3.Up hab ich gemacht, weil der Pivot Punkt des Gegners an seinen Füßen ist und der des SPielers in der Mitte***/
 
-			Debug.DrawRay(transform.position+transform.up, direction.normalized*col.radius, Color.green); /***gut zum debuggen, 
-			hatte ich vergessen, dass es die Methode noch gibt ;) ***/
+//			Debug.DrawRay(transform.position+transform.up, direction.normalized*col.radius, Color.green); /***gut zum debuggen, 
+//			hatte ich vergessen, dass es die Methode noch gibt ;) ***/
 
 			float angle = Vector3.Angle (direction, transform.forward);
 			// If the angle between forward and where the player is, is less than half the angle of view...
-			if (angle < fieldOfViewAngle * 0.5f) 
-			{
+			if (angle < fieldOfViewAngle * 0.5f) {
 				RaycastHit hit;
-				//Debug.Log ("in Field of View");
-				// ... and if a raycast towards the player hits something...
-				//Debug.DrawRay(transform.position + transform.up +transform.forward, direction.normalized*10);
-				//if(Physics.Raycast(transform.position + transform.up +transform.forward, direction.normalized*10, out hit, col.radius))
-				//if (Physics.Raycast (transform.position + transform.up, direction.normalized, out hit, col.radius)) {
-				if (Physics.Raycast (transform.position + Vector3.up, direction.normalized, out hit/*, col.radius infinity?*/)) /*** +transform.up oder +Vector.up, 
-				sollte eigentlich egal sein welches von beiden man nimmt ***/
-				{ 
+				if (Physics.Raycast (transform.position + Vector3.up, direction.normalized, out hit/*, col.radius infinity?*/)) { 
 
 					// ... and if the raycast hits the player...
 					//Debug.Log (hit.collider.gameObject);
 
-					if (hit.collider.gameObject == player.gameObject) 
-					{
+					if (hit.collider.gameObject == player.gameObject) {
 						// ... the player is in sight.
 						playerInSight = true;
-						//transform.LookAt (player.transform.position);/*** konnte komplett raus, der Agent macht das automatisch***/
-						//Debug.Log ("And SEE you");
-						agent.destination = player.transform.position; /*** An dieser Stelle noch das neue Target setzen. 
-						Sonst wird das bis jetzt nirgends getan (Zumindest soweit ich gesehn habe***/
-						if(Vector3.Distance(player.transform.position,transform.position)<2.0f)
-						{
-							gameController.endGameWithLoose(); //Besser in der Start() schon suchen
+						agent.destination = player.transform.position; 
+						if (Vector3.Distance (player.transform.position, transform.position) < 2.0f) {
+							gameController.endGameWithLoose (); //Besser in der Start() schon suchen
 						}
 					}
 				} 
-//				else 
-//				{
-//					Debug.Log ("But something is in the way");
-//					
-//				}
 			}
+		}
+
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.tag == "Player") {
+//			stopSounds();
 		}
 	}
 }
